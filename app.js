a540f6708cfcba9ed0bb8200709765c448525c10
GuJialu <msljtacslw@gmail.com>
2019-06-04 17:09:54 -0400

    add database connection

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const Koa = require('koa');
 var bcrypt = require('bcrypt');
 const Jwt = require('jsonwebtoken');
+const mysql = require('mysql2');
 const AWS = require('aws-sdk') // make sure you have the credentials file in C:\Users\<Username>\.aws\ otherwise it won't connect
 
 const app = new Koa();
+
+const pool = mysql.createPool({
+    host: 'cs407jialudb.c0ughbdye66r.us-east-2.rds.amazonaws.com',
+    user: 'cs407jialu',
+    database: 'cs407dba',
+    password: 'cs407pass',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+const promisePool = pool.promise()
 
 const s3 = new AWS.S3({
     signatureVersion: 'v4',
@@ -23,6 +35,10 @@ app.use(async (ctx, next) => {
     if (ctx.method === 'GET' && ctx.path === '/login'){
 
         //check username and password not implemented yet
+        const dbres = await promisePool.execute(
+            'create table users(Email varchar(255) not null, Pass varchar(255) not null, Username varchar(255) not null, primary key(Email));'
+            );
+        console.log(dbres);
 
         token = Jwt.sign({userA}, appsecret);
         ctx.body = token;
@@ -61,14 +77,14 @@ app.use(async (ctx, next) => {
 });
 */
 
-const myBucket = 'cs407projectjialu'
-const myKey = 'workingspace.zip'
-const signedUrlExpireSeconds = 60 * 5
+const myBucket = 'cs407projectjialu' // bucket name, don't change
+const myKey = 'workingspace.zip' // Key is the id of the object, it should be <name provided by the user> + delimiter + uuid5 hased user email
+const signedUrlExpireSeconds = 60 * 5 //how long you want the url to be valid
 
 
 var params = {Bucket: myBucket, Key: myKey};
-const urlUpload = s3.getSignedUrl('putObject', params); // the upload url to aws s3, use put request to upload
-const urlDownload = s3.getSignedUrl('getObject', params); // the download url to s3, use get request to download
-console.log(urlUpload);
+//const urlUpload = s3.getSignedUrl('putObject', params); // the upload url to aws s3, use put request to upload
+//const urlDownload = s3.getSignedUrl('getObject', params); // the download url to aws s3, use get request to download
+//console.log(urlUpload);
 
 app.listen(3000);
