1b9c77ab6ee0409f7e8e31b31590ac13f8c935d4
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-06-13 16:40:24 -0400

    reset koa server

--- app.js
+++ app.js
@@ -1,95 +1,44 @@
-const Koa = require('koa');
-const bcrypt = require('bcrypt');
-const Jwt = require('jsonwebtoken');
-const mysql = require('mysql2');
-const AWS = require('aws-sdk'); // make sure you have the credentials file in C:\Users\<Username>\.aws\ otherwise it won't connect
-const json = require('koa-json');
-const onerror = require('koa-onerror');
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
 
-const app = new Koa();
+const index = require('./routes/index')
+const users = require('./routes/users')
 
-// const pool = mysql.createPool({
-//     host: 'cs407jialudb.c0ughbdye66r.us-east-2.rds.amazonaws.com',
-//     user: 'cs407jialu',
-//     database: 'cs407dba',
-//     password: 'cs407pass',
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-const mysqlCredential = require('./config/dev').mysql['aws'];
-const pool = mysql.createPool(mysqlCredential);
-const promisePool = pool.promise()
+// error handler
+onerror(app)
 
-const s3 = new AWS.S3({
-    signatureVersion: 'v4',
-    region: 'us-east-2'
-});
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
 
-const appsecret = 'secret'; //hard coded secret
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
 
-app.use(async(ctx, next) => {
-
-    const userA = {
-        id: 0,
-        username: 'aaa',
-        email: 'aaa@gmail.com'
-    }
-
-    if (ctx.method === 'GET' && ctx.path === '/login') {
-
-        //check username and password not implemented yet
-        const dbres = await promisePool.execute(
-            'create table users(Email varchar(255) not null, Pass varchar(255) not null, Username varchar(255) not null, primary key(Email));'
-        );
-        console.log(dbres);
-
-        token = Jwt.sign({ userA }, appsecret);
-        ctx.body = token;
-        return;
-    }
-
-    if (ctx.method === 'GET' && ctx.path === '/getprotected') {
-
-        //check the authorization when the client wants to request upload and download
-        if (!tokenIsVaild(ctx)) {
-            ctx.status = 403;
-            return;
-        }
-        ctx.body = 'protectedcontent';
-        return;
-    }
-});
-
-//function verifies if the authrization token from the client is vaild
-function tokenIsVaild(ctx) {
-    bearerHeader = ctx.header['authorization'];
-    token = bearerHeader.split(' ')[1];
-    console.log(token);
-    try {
-        Jwt.verify(token, appsecret);
-    } catch (e) {
-        console.log(e);
-        return false;
-    }
-    return true;
-}
-
-/*
+// logger
 app.use(async (ctx, next) => {
-    console.log("hellow1");
-    ctx.body = 'Hello World';
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
 });
-*/
 
-const myBucket = 'cs407projectjialu' // bucket name, don't change
-const myKey = 'workingspace.zip' // Key is the id of the object, it should be <name provided by the user> + delimiter + uuid5 hashed user email
-const signedUrlExpireSeconds = 60 * 5 //how long you want the url to be valid
-
-
-var params = { Bucket: myBucket, Key: myKey };
-//const urlUpload = s3.getSignedUrl('putObject', params); // the upload url to aws s3, use put request to upload
-//const urlDownload = s3.getSignedUrl('getObject', params); // the download url to aws s3, use get request to download
-//console.log(urlUpload);
-
-app.listen(3000);
+module.exports = app
